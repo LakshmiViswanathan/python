8602cdf07030651c649ca815d794413f49d45722
nadiyaravi <nadiyaravi916@gmail.com>
2022-12-14 02:18:15 +0530

    contact updated

--- flat/flat/views.py
+++ flat/flat/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, HttpResponse,redirect
 from service.models import Service
+from contact.models import Contact
 
 
 def index(request):
     # return HttpResponse('Hello')
     s=Service.objects.all()
     return render(request, 'index.html', {'s':s})
+
+def contact(request):
+    # return HttpResponse('Hello')
+    return render(request, 'contact.html')
+
+def handle_contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        msg=request.POST.get('msg')
+        c= Contact(name=name, email=email, subject=subject, message=msg)
+        c.save()
+        return redirect('contact')
 
 def service(request):
     # return HttpResponse('Hello')
