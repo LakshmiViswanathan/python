e2fa0cebc390f4ba32f5722975ff0c8c89b5c406
nadiyaravi <nadiyaravi916@gmail.com>
2022-12-14 02:02:00 +0530

    booking update model

--- flat/booking/models.py
+++ flat/booking/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Contact(models.Model):
-    name=models.CharField(max_length=20)
-    email=models.EmailField()
-    subject=models.TextField(max_length=20)
-    message=models.TextField()
