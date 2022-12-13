54c4a8071d86f34f58df6c6b9fc3305d036f54c6
LakshmiViswanathan <117992466+LakshmiViswanathan@users.noreply.github.com>
2022-12-13 17:11:38 +0000

    service

--- flat/flat/views.py
+++ flat/flat/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, HttpResponse,redirect
-
+from service.models import Service
 
 
 def index(request):
-    # s=Service.objects.all()
-    # r = Room.objects.all()
     # return HttpResponse('Hello')
-    return render(request, 'index.html')
+    s=Service.objects.all()
+    return render(request, 'index.html', {'s':s})
+
+def service(request):
+    # return HttpResponse('Hello')
+    s=Service.objects.all()
+    return render(request, 'service.html', {'s':s})
+
+def show_service(request):
+    # return HttpResponse('Hello')
+    if request.method=='POST':
+        pk=request.POST.get('pk')
+        s=Service.objects.get(pk=pk)
+        return render(request, 'show_service.html', {'s':s}) 
--- flat/service/models.py
+++ flat/service/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    name=models.CharField(max_length=50)
+    head=models.CharField(max_length=100)
+    desc=models.TextField()
+    image= models.ImageField(upload_to='service', default='abc')
